77f470079994c2ce5ae6d4f5cc89f01f8511381a
namsh1125 <namsh1125@naver.com>
2025-03-02 18:49:14 +0900

    🔧 OpenFeign 설정 추가

---
+++ src/main/java/site/haruhana/www/config/OpenFeignConfig.java
@@ -0,0 +1,10 @@
+package site.haruhana.www.config;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "site.haruhana.www.feign")
+public class OpenFeignConfig {
+
+}
