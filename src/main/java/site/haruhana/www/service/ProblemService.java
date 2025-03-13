1a2f827c79ab51a3566215fc931f6e105914ee20
namsh1125 <namsh1125@naver.com>
2025-03-13 11:37:57 +0900

    ♻️ 랜덤 값 생성 기능 RandomUtil 클래스로 분리

--- src/main/java/site/haruhana/www/service/ProblemService.java
+++ src/main/java/site/haruhana/www/service/ProblemService.java
@@ -9,8 +9,7 @@ import site.haruhana.www.entity.problem.ProblemCategory;
 import site.haruhana.www.entity.problem.ProblemDifficulty;
 import site.haruhana.www.entity.problem.Problem;
 import site.haruhana.www.repository.ProblemRepository;
-
-import java.util.Random;
+import site.haruhana.www.utils.RandomUtil;
 
 @Slf4j
 @Service
@@ -20,7 +19,7 @@ public class ProblemService {
     // 의존성 주입
     private final AIService aiService;
     private final ProblemRepository problemRepository;
-    private final Random random = new Random();
+    private final RandomUtil randomUtil;
 
     /**
      * 60초마다 실행되는 문제 자동 생성 스케줄러
@@ -42,9 +41,9 @@ public class ProblemService {
     public void generateRandomProblem() {
         try {
             // 랜덤 카테고리, 난이도, 문제 유형 선택
-            ProblemCategory randomCategory = getRandomCategory();
-            ProblemDifficulty randomDifficulty = getRandomDifficulty();
-            boolean isMultipleChoice = random.nextBoolean();
+            var randomCategory = randomUtil.getRandomCategory();
+            var randomDifficulty = randomUtil.getRandomDifficulty();
+            var isMultipleChoice = randomUtil.getRandomBoolean();
 
             // 문제 생성
             Problem problem = isMultipleChoice ?
@@ -59,25 +58,5 @@ public class ProblemService {
         } catch (Exception e) {
             log.error("문제 자동 생성 중 오류 발생: {}", e.getMessage());
         }
-    }
-
-    /**
-     * 랜덤한 문제 카테고리를 선택하는 메소드
-     *
-     * @return 무작위로 선택된 {@link ProblemCategory}
-     */
-    private ProblemCategory getRandomCategory() {
-        ProblemCategory[] categories = ProblemCategory.values();
-        return categories[random.nextInt(categories.length)];
-    }
-
-    /**
-     * 랜덤한 문제 난이도를 선택하는 메소드
-     *
-     * @return 무작위로 선택된 {@link ProblemDifficulty}
-     */
-    private ProblemDifficulty getRandomDifficulty() {
-        ProblemDifficulty[] difficulties = ProblemDifficulty.values();
-        return difficulties[random.nextInt(difficulties.length)];
     }
 }
