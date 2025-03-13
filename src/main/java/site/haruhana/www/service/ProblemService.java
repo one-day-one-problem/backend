347b75e16e12e8e89cd5645e81f65a936eb559ad
namsh1125 <namsh1125@naver.com>
2025-03-13 12:43:17 +0900

    ♻️ 문제 생성 로그 개선

--- src/main/java/site/haruhana/www/service/ProblemService.java
+++ src/main/java/site/haruhana/www/service/ProblemService.java
@@ -53,7 +53,7 @@ public class ProblemService {
             // 문제 저장
             problemRepository.save(problem);
 
-            log.info("새로운 문제가 생성되었습니다. 유형: {}, 카테고리: {}, 난이도: {}", problemType, randomCategory, randomDifficulty);
+            log.info("새로운 문제가 생성되었습니다. 유형: {}, 카테고리: {}, 난이도: {}", problemType.getDescription(), randomCategory, randomDifficulty);
 
         } catch (Exception e) {
             log.error("문제 자동 생성 중 오류 발생: {}", e.getMessage());
