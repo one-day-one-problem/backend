213fdeb27d9ea392f4e0fa46ead8ee9d7f2165e3
namsh1125 <namsh1125@naver.com>
2025-03-16 14:07:04 +0900

    🚚 TokenDto 패키지 이동 (dto -> dto/user) 및 관련 import 문 수정

--- src/main/java/site/haruhana/www/oauth/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/site/haruhana/www/oauth/handler/OAuth2LoginSuccessHandler.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import site.haruhana.www.base.BaseResponse;
-import site.haruhana.www.dto.TokenDto;
+import site.haruhana.www.dto.user.TokenDto;
 import site.haruhana.www.oauth.CustomOAuth2User;
 import site.haruhana.www.utils.JwtUtil;
 
