130ecb6cad554c1ae750c03aaef3e9ac262b992b
namsh1125 <namsh1125@naver.com>
2025-03-31 11:26:22 +0900

    ♻️ ProblemDto 클래스를 추상 클래스로 변경하고, 문제 유형에 따라 상속된 DTO로 변환하도록 수정

---
+++ src/main/java/site/haruhana/www/dto/problem/extend/SubjectiveProblemDto.java
@@ -0,0 +1,17 @@
+package site.haruhana.www.dto.problem.extend;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import site.haruhana.www.dto.problem.ProblemDto;
+import site.haruhana.www.entity.problem.Problem;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SubjectiveProblemDto extends ProblemDto {
+    private final String expectedAnswerLength;
+
+    public SubjectiveProblemDto(Problem problem, Boolean isSolved) {
+        super(problem, isSolved);
+        this.expectedAnswerLength = problem.getExpectedAnswerLength();
+    }
+}
