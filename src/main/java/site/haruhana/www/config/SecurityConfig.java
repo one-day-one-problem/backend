ebbcec59e44dd2a39ec640c43591fa51d9482127
namsh1125 <namsh1125@naver.com>
2025-04-08 00:48:17 +0900

    ✨ Spring Security 메서드 보안 기능 활성화

--- src/main/java/site/haruhana/www/config/SecurityConfig.java
+++ src/main/java/site/haruhana/www/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package site.haruhana.www.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -19,6 +20,7 @@ import site.haruhana.www.oauth.handler.OAuth2LoginSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
